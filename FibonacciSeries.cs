5716ecdbf28b9b41347444410bfd399ab740bdc2
poojakrp777 <poojakrp777@gmail.com>
2023-01-30 22:25:31 +0530

    [POOJA] Fibonacci series Logical Program

---
+++ FibonacciSeries.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class FibonacciSeries
+    {
+        public static void FindSeries(int n)
+        {
+            //Initilazing the variables
+            int first = 0;
+            int second = 1;
+            int sum = 0;
+            Console.Write("{0} {1}", first, second);
+            //Starts the loop from 2 because 0 and 1 are already printed
+            for (int i = 2; i < n; i++)
+            {
+                sum = first + second;
+                Console.Write(" {0}", sum);
+                first = second;
+                second = sum;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please choose any one program from below options");
+            Console.WriteLine("1:Fibonacciseries");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            switch (choose)
+            {
+                case 1:
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    FibonacciSeries.FindSeries(n);
+                    break;
+            }
+        }
+    }
+}
